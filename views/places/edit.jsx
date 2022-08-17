0867bac5b793f705dfc3457d4f7311400bb62ca2
gavvahar <nihargavva@gmail.com>
2022-08-16 20:34:56 -0400

    Added the edit form

---
+++ views/places/edit.jsx
@@ -0,0 +1,36 @@
+const React = require('react')
+const Def = require('../default.jsx')
+
+function edit_form (data) {
+    return (
+        <Def>
+          <main>
+            <h1>Edit Place</h1>
+          </main>
+          <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
+  <div className="row">
+      <div className="form-group col-sm-6">
+          <label htmlFor="name">Place Name</label>
+          <input
+          className='form-control'
+          id="name"
+          name='name'
+          value={data.place.name}
+          required/>
+      </div>
+      <div className="form-group col-sm-6">
+          <label htmlFor="pic">Place Picture</label>
+          <input
+          className='form-control'
+          id="pic"
+          name='pic'
+          value={data.place.pic}
+          required/>
+      </div>
+  </div>
+</form>
+        </Def>
+    )
+}
+
+module.exports = edit_form
