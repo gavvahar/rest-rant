4ec0feb518ac581918e0b9d9141c27fdb5283f6d
gavvahar <nihargavva@gmail.com>
2022-08-24 19:03:30 -0400

    Adding comment section to the show page

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,7 +2,25 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
-    return (
+  let comments = (
+    <h3 className='inactive'>No comments yet!</h3>
+  )
+  if(data.place.comments.length)
+  {
+    comments = data.place.comments.map(c => {
+      return (
+        <div className='border'>
+          <h2 className='rant'>{c.rant ? 'Rant! 😒': 'Rave! 😻'}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <strong>- {c.author}</strong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      )
+    })
+  }
+  return (
         <Def>
           <main>
             <h1>{ data.place.name }</h1>
@@ -17,6 +35,8 @@ function show (data) {
                 <h2>Description</h2>
                 <h3>{data.place.showEstablished()}</h3>
                 <h4>Serving {data.place.cuisines}</h4>
+                <h2>Comments</h2>
+                {comments}
               </div>
             </div>
           </main>
